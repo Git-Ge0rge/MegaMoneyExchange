8b35f189c814c59408077708f5a3ad4e422f5ea2
ahmxdz <ahmedali6342@gmail.com>
2023-01-08 16:50:26 -0500

    page setup

---
+++ mme-website/src/pages/ContactsAdmin/ContactsAdmin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ContactsAdmin() {
+  return (
+    <div>ContactsAdmin</div>
+  )
+}
+
+export default ContactsAdmin
---
+++ mme-website/src/pages/ReportsAdmin/ReportsAdmin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ReportsAdmin() {
+  return (
+    <div>ReportsAdmin</div>
+  )
+}
+
+export default ReportsAdmin
