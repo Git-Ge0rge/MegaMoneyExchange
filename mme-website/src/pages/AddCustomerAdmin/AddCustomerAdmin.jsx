4e90e90ae92be63360f899344492ebb6ac1cc128
ahmxdz <ahmedali6342@gmail.com>
2023-01-08 19:29:11 -0500

    routing done

---
+++ mme-website/src/pages/AddCustomerAdmin/AddCustomerAdmin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddCustomerAdmin() {
+  return (
+    <div>AddCustomerAdmin</div>
+  )
+}
+
+export default AddCustomerAdmin
--- mme-website/src/pages/App/App.js
+++ mme-website/src/pages/App/App.js
@@ -1,9 +1,32 @@
+import NavBar from '../../components/NavBar';
+import {Route, Routes} from "react-router-dom";
+import AdminHome from '../AdminHome/AdminHome';
+import ReportsAdmin from '../ReportsAdmin/ReportsAdmin';
+import ContactsAdmin from '../ContactsAdmin/ContactsAdmin';
+import ViewCustomerAdmin from '../ViewCustomerAdmin/ViewCustomerAdmin';
+import AddCustomerAdmin from '../AddCustomerAdmin/AddCustomerAdmin';
+import SendMoneyAdmin from '../SendMoneyAdmin/SendMoneyAdmin';
+import ViewOrdersAdmin from '../ViewOrdersAdmin/ViewOrdersAdmin';
+import OfferRateAdmin from '../OfferRateAdmin/OfferRateAdmin';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/" element={<AdminHome/>}/>
+        <Route path="reports" element={<ReportsAdmin/>}/>
+        <Route path="contacts" element={<ContactsAdmin/>}/>
+        <Route path="view-customer" element={<ViewCustomerAdmin/>}/>
+        <Route path="add-customer" element={<AddCustomerAdmin/>}/>
+        <Route path="send-money" element={<SendMoneyAdmin/>}/>
+        <Route path="view-orders" element={<ViewOrdersAdmin/>}/>
+        <Route path="offer-rate" element={<OfferRateAdmin/>}/>
+
+
+
+      </Routes>
     </div>
   );
 }
