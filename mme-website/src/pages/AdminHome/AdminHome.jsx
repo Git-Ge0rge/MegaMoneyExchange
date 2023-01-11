b47099f7e004aebd2d4d3ebdb1935df8b16c890c
ahmxdz <ahmedali6342@gmail.com>
2023-01-11 17:27:03 -0500

    css

--- mme-website/src/pages/AdminHome/AdminHome.jsx
+++ mme-website/src/pages/AdminHome/AdminHome.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./AdminHome.css"
 
 function AdminHome() {
   return (
-    <div>AdminHome</div>
+    <div>
+      <div className="container-homepage">
+      <div className="top-tables-homepage">Weekly Reports</div>
+      <div className="top-tables-homepage2"> Conversion Table</div>
+      {/* <div className="paki-news">Pakistani News</div>
+      <div className="paki-reports">Pakistani Reports </div> */}
+      </div>
+
+
+
+
+
+
+    </div>
   )
 }
 
