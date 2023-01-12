ccd2f06a163a1fb3757c8c7ea099ecd3a7a587fe
ahmxdz <ahmedali6342@gmail.com>
2023-01-11 21:42:00 -0500

    css

--- mme-website/src/pages/AdminHome/AdminHome.jsx
+++ mme-website/src/pages/AdminHome/AdminHome.jsx
@@ -5,10 +5,10 @@ function AdminHome() {
   return (
     <div>
       <div className="container-homepage">
-      <div className="top-tables-homepage">Weekly Reports</div>
-      <div className="top-tables-homepage2"> Conversion Table</div>
-      {/* <div className="paki-news">Pakistani News</div>
-      <div className="paki-reports">Pakistani Reports </div> */}
+        <div className="top-tables-homepage">Weekly Reports</div>
+        <div className="top-tables-homepage2"> Conversion Table</div>
+        {/* <div className="paki-news">Pakistani News</div>
+        <div className="paki-reports">Pakistani Reports </div> */}
       </div>
 
 
